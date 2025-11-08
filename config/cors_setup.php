82639b27985bb3ed01e397545354fe4adecae8d2
hlovs65 <salvadorlopezvillafan@gmail.com>
2025-11-08 02:09:08 -0600

    Fix: Eliminar funcion que limpia el búfer de salida.

--- config/cors_setup.php
+++ config/cors_setup.php
@@ -1,18 +1,19 @@
 <?php
+// Usamos el operador @ para suprimir errores si la cabecera ya se envió o falla. 
 // Define la URL de tu frontend de Vercel
+
 $allowed_origin = 'https://galeria-app-frontend.vercel.app'; 
 
 // Establecer cabeceras CORS
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Credentials: true"); // Permitir el envío de cookies y credenciales
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Permitir métodos POST y OPTIONS
-header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Permitir encabezados específicos
-header("Content-Type: application/json; charset=UTF-8");
+@header("Access-Control-Allow-Origin: $allowed_origin");
+@header("Access-Control-Allow-Credentials: true"); // Permitir el envío de cookies y credenciales
+@header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Permitir métodos POST y OPTIONS
+@header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Permitir encabezados específicos
+@header("Content-Type: application/json; charset=UTF-8");
 
 // Manejar la solicitud OPTIONS (preflight request)
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
-    if (ob_get_level()) { ob_end_clean(); } // Limpiar el búfer de salida si es necesario
     exit();
 }
 ?>
