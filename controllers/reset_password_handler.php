54a27f38e3ed12fd49ad8676c6741b8db1067a90
hlovs65 <salvadorlopezvillafan@gmail.com>
2025-11-29 02:13:33 -0600

    añade: Encabezados CORS.

--- controllers/reset_password_handler.php
+++ controllers/reset_password_handler.php
@@ -1,4 +1,16 @@
 <?php
+// ----------------------------------------------------
+// ✅ BLOQUE CORS AÑADIDO (¡CRÍTICO!)
+// ----------------------------------------------------
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS'); 
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+// ----------------------------------------------------
 if (ob_get_level()) {
     ob_clean(); 
 }
